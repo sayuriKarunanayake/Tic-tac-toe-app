50c1c999da524cb2b08355b0124d5a72f196e747
Sayuri <it20197032@my.sliit.lk>
2022-09-14 14:27:09 +0530

    game logic implementation

--- tic-tac-toe/src/components/Square.js
+++ tic-tac-toe/src/components/Square.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 export default function Square(props) {
   return (
-    <button className='btn' onClick={props.onClick}>
+    <button 
+    className={props.value ? "btn disabled" : "btn" } 
+    onClick={props.onClick}>
         {props.value}
     </button>
   )
