09f669f89ee065bb5fe1346e0fe594dbc6b5f152
Sayuri <it20197032@my.sliit.lk>
2022-10-11 14:51:28 +0530

    comments added

--- tic-tac-toe/src/components/Game.js
+++ tic-tac-toe/src/components/Game.js
@@ -6,41 +6,42 @@ const reducer = (state, action)=>{
   switch(action.type) {
     case 'JUMP' :
       return{
-        ...state,
+        ...state,//keep prev. state
         xIsNext: action.payload.step % 2 === 0,
         history: state.history.slice(0, action.payload.step + 1),
       };
-    case 'MOVE' :
+    case 'MOVE' ://if a move happens return a new state
       return{
         ...state,
-        history: state.history.concat({
+        history: state.history.concat({//history within state concat with new one
           squares:action.payload.squares,
         }),
-        xIsNext:!state.xIsNext,
+        xIsNext:!state.xIsNext,//next player 
       };
       default:
-        return state;
+        return state;//return current state 
   }
 };
 
 export default function Game() {
   const [state, dispatch] = useReducer(reducer, {
     /*default state */
-    xIsNext: true,
-    history:[{squares: Array(9).fill(null)}]
+    xIsNext: true,//1st player is X
+    history:[{squares: Array(9).fill(null)}] //arrays of arrays
   });
   const {xIsNext, history} = state;
   const jumpTo = (step)=>{
     dispatch({type:'JUMP', payload:{step}});
   };
+  
   const handleClick = (i) => {
-    const current = history[history.length-1];
-    const squares = current.squares.slice();
-    const winner = calWinner(squares);
-    if(winner || squares[i]){
+    const current = history[history.length-1];//get the last item in the history
+    const squares = current.squares.slice();//copy of squares in a new variable
+    const winner = calWinner(squares);//get winner
+    if(winner || squares[i]){//if winner is exact or square is occupied  
       return;
     }
-    squares[i]= xIsNext? 'X': '0';
+    squares[i]= xIsNext? 'X': '0';//fill square 
     dispatch({type:'MOVE', payload: {squares}});
   };
   const current = history[history.length-1];
@@ -49,7 +50,8 @@ export default function Game() {
   const status = winner? winner=== 'D'?'Draw': 'Winner is ' + winner: 'Next player is ' + (xIsNext? 'X' : '0'); 
 
   const moves = history.map((step,move)=>{
-    const desc = move? 'Go to #' + move: 'Start the Game';
+    const desc = move? 'Go to #' + move: 'Start the Game';//if move is 0 start the game otherwise render go to #
+    //btn use to move in btw history 
     return <li key={move}>
       <button onClick={()=>jumpTo(move)}>
         {desc}
@@ -74,7 +76,7 @@ export default function Game() {
 }
 
 const calWinner = (squares) => {
-  const winnerLines = [
+  const winnerLines = [ //possible lines for winning the game
     [0,1,2],
     [3,4,5],
     [6,7,8],
@@ -88,11 +90,11 @@ const calWinner = (squares) => {
   for(let i=0; i < winnerLines.length; i++){
     const[a, b, c] = winnerLines[i];
     
-    if(squares[a] && squares[a] === squares[b] && squares[b] === squares[c]){
-      return squares[a];
+    if(squares[a] && squares[a] === squares[b] && squares[b] === squares[c]){//compare the current state with winnerLines 
+      return squares[a];//have a winner
     }
     if(!squares[a] || !squares[b] || !squares[c]){
-      isDraw = false;
+      isDraw = false;//no draw cuz we have room to continue the game
     }
   }
   if(isDraw) return 'D';
